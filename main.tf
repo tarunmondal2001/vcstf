fc8083c59050010c619fc11e9fdd27e3275f9e42
Vijay K <sharmavijay86@gmail.com>
2023-09-23 11:50:35 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+  required_version = "~> 1.5.6"
+}
+
+provider "aws" {
+region =  "us-east-2"
+}
+resource "aws_vpc" "main" {
+  cidr_block = "192.168.10.0/24"
+}
